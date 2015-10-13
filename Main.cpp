480190087cc6697dec59d42ea374dd235c9fe1f8
Brett Allen <brettallen777@gmail.com>
2015-10-13 15:42:38 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -11,22 +11,27 @@ Finally the program will determine the entire class' average and display everyth
 
 using namespace std;
 
+//Constants
+const int SIZE = 30;
+
 //Prototypes
 void highestGrade(string name[], double classAvg[], int size);
 void lowestGrade(string name[], double classAvg[], int size);
 void classAverage(double classAvg[], int size);
 void letterGrade(string name[], double classAvg[], char letterGrade[], int size);
-
-const int SIZE = 30;
+void showStudents(int tally[], string marker[][SIZE], char letterGrade[]);
 
 int main()
 {
+	//Setting color scheme to white on black for readability
+	system("color F0");
+
 	ifstream inputFile;
 	inputFile.open("lab5.txt");	
 
 	string name[SIZE];
 	double classAvg[SIZE];
-	char grade[] = { 'A', 'B', 'C', 'D', 'F' };
+	char grade[] = { 'A', 'B', 'C', 'D', 'F' };	
 
 	//Reading data
 	while (!inputFile.eof())
@@ -53,6 +58,8 @@ int main()
 }
 
 //Function Definitions
+
+//Searching the parallel arrays name and classAvg for the student w/highest average
 void highestGrade(string name[], double classAvg[], int size)
 {
 	double hGrade = classAvg[0];
@@ -70,6 +77,7 @@ void highestGrade(string name[], double classAvg[], int size)
 	cout << hName << " has the highest class average with a " << hGrade  << "%" << endl;
 }
 
+//Searching the parallel arrays name and classAvg for the student w/lowest average
 void lowestGrade(string name[], double classAvg[], int size)
 {
 	double lGrade = classAvg[0];
@@ -87,6 +95,7 @@ void lowestGrade(string name[], double classAvg[], int size)
 	cout << lName << " has the lowest class average with a " << lGrade << "%" << endl;
 }
 
+//Using the classAvg array and its size to determine the entire class average
 void classAverage(double classAvg[], int size)
 {
 	double sum = 0.0;
@@ -97,37 +106,69 @@ void classAverage(double classAvg[], int size)
 		sum += classAvg[i];
 	}
 
-	cout << "The class average is a " << fixed << setprecision(1) << sum / size << "%\n";
+	cout << "Overall, the entire class average is a " << fixed << setprecision(1) << sum / size << "%\n";
 }
 
+//Determining how many students received each letter grade and naming each student that has
+//each letter grade
 void letterGrade(string name[], double classAvg[], char letterGrade[], int size)
 {
+	int tally[] = { 0, 0, 0, 0, 0 };	
+	string marker[5][SIZE];
+
+	//Determining letter grade based on student average
 	for (int i = 0; i < size; i++)
 	{
 		if (classAvg[i] >= 89.5 && classAvg[i] <= 100)
-		{
-			cout << name[i] << " has a(n) " << letterGrade[0] << " at a " <<
-				classAvg[i] << "%\n\n"; //Student has an 'A'
+		{			
+			marker[0][tally[0]] = name[i];
+			tally[0]++;
 		}
 		else if (classAvg[i] >= 79.5)
-		{
-			cout << name[i] << " has a(n) " << letterGrade[1] << " at a " <<
-				classAvg[i] << "%\n\n"; //Student has a 'B'
+		{			
+			marker[1][tally[1]] = name[i];
+			tally[1]++;
 		}
 		else if (classAvg[i] >= 69.5)
 		{
-			cout << name[i] << " has a(n) " << letterGrade[2] << " at a " <<
-				classAvg[i] << "%\n\n"; //Student has a 'C'
+			marker[2][tally[2]] = name[i];
+			tally[2]++;
 		}
 		else if (classAvg[i] >= 59.5)
 		{
-			cout << name[i] << " has a(n) " << letterGrade[3] << " at a " <<
-				classAvg[i] << "%\n\n"; //Student has a 'D'
+			marker[3][tally[3]] = name[i];
+			tally[3]++;
 		}
 		else if (classAvg[i] >= 0)
 		{
-			cout << name[i] << " has a(n) " << letterGrade[4] << " at a " <<
-				classAvg[i] << "%\n\n"; //Student has an 'E'
+			marker[4][tally[4]] = name[i];
+			tally[4]++;
+		}
+	}
+
+	showStudents(tally, marker, letterGrade);	
+}
+
+//Displaying how man students have each individual letter grade along with a list
+//of coinciding names
+void showStudents(int tally[], string marker[][SIZE], char letterGrade[])
+{
+	for (int row = 0; row < 5; row++)
+	{		
+		cout << tally[row] << " students have " << letterGrade[row] << "\'s. They are:\n";
+
+		for (int col = 0; col < SIZE; col++)
+		{
+			if (marker[row][col] != "")
+			{
+				cout << "\t" << marker[row][col] << endl;
+			}
+			else
+			{
+				//Terminating loop once a blank element is found in the 2D marker array 
+				cout << endl;
+				break;
+			}
 		}
 	}
 }
